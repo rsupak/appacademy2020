3330857c595326e4660f1a5e70e2885b33fd086d
Richard Supak <richard.supak@icloud.com>
2020-09-13 21:22:21 -0500

    Add solutions for associations_exercise

---
+++ rails/associations_exercise/app/models/course.rb
@@ -0,0 +1,32 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :bigint           not null, primary key
+#  name          :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  instructor_id :integer
+#  prereq_id     :integer
+#
+class Course < ApplicationRecord
+
+  has_many :enrollments,
+    primary_key: :id,
+    foreign_key: :course_id,
+    class_name: :Enrollment
+
+  has_many :enrolled_students,
+    through: :enrollments,
+    source: :user
+
+  has_one :prerequisite,
+    primary_key: :prereq_id,
+    foreign_key: :id,
+    class_name: :Course
+
+  has_one :instructor,
+    primary_key: :instructor_id,
+    foreign_key: :id,
+    class_name: :User
+end
